d6840f12825bfd1787c90675d57b0c6032e0d6cc
usmanlubis <usmanlubis11@gmail.com>
2022-07-09 19:53:09 +0700

    update string method

--- frontend/javascript/1-basic/4-string.js
+++ frontend/javascript/1-basic/4-string.js
@@ -4,6 +4,13 @@ let age = 23;
 let fullName = firstName + " " + lastName;
 console.log(fullName);
 
+// escape character
+console.log('I\'am Groot'); // I'am Groot
+console.log("She said \"yes\""); // She said "yes"
+console.log("\\"); // \ (backslash)
+console.log("usman \t lubis"); // usman   lubis (have tab)
+// you can find more other escape characters
+
 // without literal template
 console.log("Hello, my name is " + fullName + ", and I am " + age + " years old.");
 
@@ -14,5 +21,22 @@ console.log(`Hello, my name is ${fullName}, and I am ${age} years old.`);
 console.log(fullName[0]);
 console.log(fullName[8]);
 
+// javascript method and properties
 // length start from 1
 console.log(fullName.length); // 11 (including space)
+
+// toupper and lower case
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+
+// include
+console.log("Mathematics".includes("hema")); // true
+
+// startsWith and endsWith
+console.log("Mathematics".startsWith("Math")); // true
+console.log("Mathematics".endsWith("ics")); // true
+
+// trim (for cropping space in start and end of stings)
+console.log("     usman lubis     ".trim()); // usman lubis
+
+// you can find more string method and properties
